aafcdc93420d15a6b6bcfc7b6551f726458e57f2
Matt Stacks <m@mstacks.dev>
2023-05-27 19:10:22 +0000

    May 27, 2023, 3:10 PM

--- src/codewars/find_next_perfect_square/find_next_square.py
+++ src/codewars/find_next_perfect_square/find_next_square.py
@@ -1,3 +1,18 @@
 def find_next_square(sq):
-    # Return the next square if sq is a square, -1 otherwise
+    """Return the next square if sq is a square, -1 otherwise
+
+    :param sq: number to check
+    :return: next square if sq is a square, -1 otherwise
+
+    >>> find_next_square(121)
+    144
+    >>> find_next_square(625)
+    676
+    >>> find_next_square(65)
+    -1
+    >>> find_next_square(319225)
+    320356
+    >>> find_next_square(0)
+    1
+    """
     return -1 if sq**0.5 % 1 else (sq**0.5 + 1) ** 2
