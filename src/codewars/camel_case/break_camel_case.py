8fb4255de59e945a7c12d9b4faec8a6119e203fe
Matt Stacks <m@mstacks.dev>
2023-05-28 00:05:35 +0000

    May 27, 2023, 8:05 PM

---
+++ src/codewars/camel_case/break_camel_case.py
@@ -0,0 +1,17 @@
+def break_camel_case(s: str) -> str:
+    """
+    :param s: string to break
+    :return: broken string
+
+    >>> break_camel_case("camelCasing")
+    "camel Casing"
+    >>> break_camel_case("camelCasingTest")
+    "camel Casing Test"
+    >>> break_camel_case("")
+    ""
+    >>> break_camel_case("camelcasingtest")
+    "camelcasingtest"
+    >>> break_camel_case("CamelCasingtest")
+    "Camel Casingtest"
+    """
+    return "".join(" " + c if c.isupper() else c for c in s)
