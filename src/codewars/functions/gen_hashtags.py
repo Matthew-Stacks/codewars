47ac083195f5cfca7245446c7f29a44f09285053
Matt Stacks <m@mstacks.dev>
2023-05-31 10:42:40 -0400

    :under_construction: added gen_hashtags.py

---
+++ src/codewars/functions/gen_hashtags.py
@@ -0,0 +1,27 @@
+import doctest
+
+
+def gen_hashtag(s: str) -> str | bool:
+    """Generate hashtag from string.
+
+    param s: string to generate hashtag from
+    return: generated hashtag
+    constraints: 1 <= len(s) <= 140
+
+    >>> gen_hashtag('')
+    False
+    >>> gen_hashtag('HelloWorldHelloWorldHelloWorldHelloWorldHelloWorldHelloWorldHelloWorldHelloWorldHelloWorldHelloWorldHelloWorldHelloWorldHelloWorldHelloWorld!')
+    False
+    >>> gen_hashtag('Do We have A Hashtag')
+    '#DoWeHaveAHashtag'
+    >>> gen_hashtag('Codewars')
+    '#Codewars'
+    """
+    hashtag_max_length: int = 140
+    if not s or len(s) > hashtag_max_length:
+        return False
+
+    return "#" + "".join(word.capitalize() for word in s.split())
+
+
+doctest.testmod()
