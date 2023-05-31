951f746dbfb7ca9eb29eba02a4e24176a2ab961f
Matt Stacks <m@mstacks.dev>
2023-05-31 14:27:24 +0000

    May 31, 2023, 10:27 AM

--- src/codewars/filter/filter.py
+++ src/codewars/filter/filter.py
@@ -1,11 +1,13 @@
-def well(x):
+def well(x: list[str]) -> str | int:
+    series_min: int = 3
     good_ideas = x.count("good")
+
     match good_ideas:
         case n if n <= 0:
             return "Fail!"
-        case n if 0 < n < 3:
+        case n if 0 < n < series_min:
             return "Publish!"
         case n:
-            if n >= 3:
+            if n >= series_min:
                 return "I smell a series!"
     return -1
