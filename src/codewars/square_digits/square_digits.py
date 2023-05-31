f7e8098adf177db0f89646adc20a98c5327cbf4d
Matt Stacks <m@mstacks.dev>
2023-05-31 14:05:00 +0000

    May 31, 2023, 10:05 AM

--- src/codewars/square_digits/square_digits.py
+++ src/codewars/square_digits/square_digits.py
@@ -2,7 +2,7 @@ import doctest
 
 
 def square_digits(num: int) -> int:
-    """
+    """For each digit in `num`, square it and return the result as a string.
     >>> square_digits(9119)
     811181
     """
